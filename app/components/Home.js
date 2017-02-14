bd7367d861e4d419016372339ba8015fbe48ed7e
ggolikov <gr.golikov@gmail.com>
2017-02-14 19:46:03 +0300

    Add promises

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
 
     render() {
         return (
-            <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+            <div className="jumbotron col-sm-12 text-center">
                 <h1>Github Battle</h1>
                 <p className='lead'>What even is a jQuery?</p>
                 <Link to='/playerOne'>
--- app/index.js
+++ app/index.js
@@ -5,3 +5,16 @@ import routes from './config/routes'
 const root = document.getElementById('app');
 
 ReactDOM.render(routes, root);
+
+const promise = new Promise((resolve, reject) => {
+    let num = 5;
+    setTimeout(() => {
+        resolve(num);
+    }, 1000);
+
+    setTimeout(() => {
+        reject(num);
+    }, 500);
+});
+
+promise.then((err) => console.log('resolved: ' + err), (result) => console.log('rejected: ' + result))
